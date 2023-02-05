973977791a385192d18042fe30f97797c1f9655a
Dropio12 <93799788+Dropio12@users.noreply.github.com>
2023-02-05 12:04:02 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+
+/*
+Online Java - IDE, Code Editor, Compiler
+
+Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
+*/
+import CLASSARRAY.Java
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        Array numbers= new Array(3);
+        numbers.print();
+        numbers.insert(10)
+        numbers.removeAT(4)
+        system.out.println(numbers.indexOf(10))
+    }
+}
